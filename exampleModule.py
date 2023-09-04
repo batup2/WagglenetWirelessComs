45b68e01bc369023a4386e1d776db06c1237f822
Ted Josephson <tdj4@illinois.edu>
2023-09-04 11:25:02 -0500

    added poll timer

--- exampleModule.py
+++ exampleModule.py
@@ -1,24 +1,41 @@
 #"module" is not the final name for this interface
-#every hive sensor should have its own module class which implements setup and
+#every data generating device (camera, microphone, thermometer) 
+#should have its own module class which implements setup and
 #poll functions
 
 import random
+import time
+
+#dummy class representing a generic sensor, not required
+class exampleSensor():
+    def read_data(self):
+        data = [random.randint(0, 9999) for i in range(0, 10)]
+        return data
 
 class exampleModule():
-    module_name = "example module" #put the name of your sensor/project here
-    example_sensor = None
+    module_name = "example" #put the name of your sensor/project here
+    sensor = None
+    polling_interval = 0.5 #time between polls in seconds
+    last_poll = time.thread_time()
+
     def __init__(self):
-        print("instance of {module name} created")
+        print(f"instance of {self.module_name} created")
 
     def setup(self):
-        example_sensor = exampleSensor()
+        self.sensor = exampleSensor()
+
+    def wrap_poll(self):
+        if (time.thread_time() - self.last_poll) >= self.polling_interval:
+            self.last_poll = time.thread_time()
+            return self.poll()
+        return None
+
 
     def poll(self):
-        sensor_data = exampleSensor.read_data()
-        return str(sensor_data)
+        sensor_data = self.sensor.read_data()
 
-
-#dummy class for demonstration purposes
-class exampleSensor():
-    def read_data():
-        return random.randint(-100, 100)
+        #test of exception handling
+        if(random.randint(0, 50) == 42):
+            raise Exception("the magic number has been found")
+        
+        return sensor_data
--- radioManager.py
+++ radioManager.py
@@ -1,3 +1,5 @@
+import sys
+import time
 #import your module
 import exampleModule
 
@@ -8,19 +10,31 @@ modules = []
 modules.append(exampleModule.exampleModule())
 
 for module in modules:
-    module.setup()
+    #always use try except when calling module functions to prevent one module
+    # from crashing the whole program
+    try:
+        module.setup()
+    except:
+        print(f"an exception occurred while setting up {module.name}")
 
 running = True
 message_length = 64
 
 while(running):
-
     transmit = ""
-
     for module in modules:
-        transmit += f"[\"{module.module_name}\",  \"{module.poll()}\"]"
+        try:
+            poll_result = module.wrap_poll()
+            if (poll_result is not None):
+                transmit += f"[\"{module.module_name}\",  \"{poll_result}\"]"
+        except:
+            e = sys.exception()
+            transmit += f"an exception occurred when polling module \"{module.module_name}\" \n{e}"
+
     
     for i in range(len(transmit) // message_length + 1):
         message = transmit[i : min( (i + 1) * message_length, len(transmit) )]
-        #rmf9x.send_with_ack(bytearray(message))
-        print(f"sending: {message}")
+
+        if message != "":
+            #rmf9x.send_with_ack(bytearray(message))
+            print(f"sending: {message}")
